c949a06166eb14995e1c0a4d7aa87174c468ec64
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-07 10:01:00 -0800

    Modified HTML

---
+++ operators and conditional statements/swtich_statement/swtich.js
@@ -0,0 +1,11 @@
+let marks = 90;
+switch (marks) {
+    case (marks >= 90):
+        console.log("Grade A");
+
+    case (marks >= 80 && marks < 90):
+        console.log("Grade B");
+
+    case (marks <= 50):
+        console.log("Fail");
+}
