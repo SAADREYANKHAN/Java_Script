da63bbfcabdf5b161a5b8a04766b6ca4ecede0af
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-07 09:47:11 -0800

    conditional statement CODE

---
+++ operators and conditional statements/conditional.js
@@ -0,0 +1,45 @@
+// let age = 18;
+// if(age >=18){
+//     console.log("you can vote")
+// }
+// if(age < 18 ){
+//     console.log("you cannot log");
+// }
+
+// let mode = "white";
+// let color;
+
+// if(mode === "dark"){
+//     color = "white";
+// }
+// if(mode === "white"){
+//     color = "dark";
+// }
+
+// console.log(color);
+
+// ODD or EVEN number;
+
+// let num = 9;
+// if(num % 2 === 0){
+//     console.log("EVEN");
+// }else{
+//     console.log("ODD");
+// }
+
+// else if conditions ;
+
+// let age = 45;
+// if(age <=18){
+//     console.log("jonior");
+// }else if(age > 60){
+//     console.log("senior");
+// }else{
+//     console.log("MIddle");
+// }
+
+// Ternary Operator;
+
+let a = 30;
+let result = a >= 15? "good" : "bad";
+console.log(result);
---
+++ operators and conditional statements/jawa.js
@@ -0,0 +1,70 @@
+// ARITHEMATIC OPERATORs
+
+// These all are binary opperator whic perfrom operation on two opperands.
+
+// let a = 10;
+// let b = 3;
+// let c;
+// c = a + b;
+// console.log("a + b = ",c);
+
+// c = a - b;
+// console.log("a - b = ",c);
+
+// c = a * b;
+// console.log("a * b = ",c);
+
+// c = a / b;
+// console.log("a / b = ",c);
+
+// // MODULUS OPPERATOR 
+
+// c = a % b;
+// console.log("a % b = ",c);
+
+// // Exponintial opperator
+
+// c = a ** b;
+// console.log("a ** b = ",c); // 10^3
+
+// these are uniopperator which prform opperation on one operand.
+// increment and decrement opperator.
+
+// let a = 10;
+// let b = 3;
+// let c;
+// console.log("a = ",a, "& b = ",b);
+
+// a++;
+// console.log("a = ",a);
+
+// b++;
+// console.log("b = ",b);
+
+//comparision opperator in java script
+
+// let a = 5;
+// let b = "5";
+
+// console.log("a == b  is ",a==b); // return true or false
+// console.log("a != b  is ",a!=b);
+
+// console.log("a === b  is ",a===b); // strickely checking also check data type and then compaire
+// console.log("a !== b  is ",a!==b);
+
+// logical opperator  = also give final result in true or false;
+
+let a = 5;
+let b = 10;
+// Logical and &&
+let cond1 = a > b;
+let cond2 = a === 7;
+console.log("cond1 && cond2 = ",cond1 && cond2);
+
+// logical 0r ||
+
+console.log("cond1 || cond2", cond1 || cond2);
+
+
+
+
