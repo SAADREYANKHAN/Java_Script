a79c6a01a8ebf4463ec124503033bb1265f536b0
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-07 09:46:17 -0800

    Data type and Variables

---
+++ data type and variables/first.js
@@ -0,0 +1,56 @@
+// let age = 100;
+// console.log(age);
+
+// age = 25;
+// console.log(age);
+
+// age = 12;
+// console.log(age);
+
+
+// var price = 45;
+// console.log(price);
+
+//  var price = 34;
+// console.log(price);
+
+// console.log(age);
+
+// const fullName = "saad";
+// console.log(fullName);
+
+// const Name = "aqsa";
+// console.log(Name);
+
+// console.log("7 preitive data types");
+
+// let num = 34;
+// console.log(num);
+
+// let digit = 123.57;
+// console.log(digit);
+
+// console.log(typeof(num));
+
+// isFollow = false;
+// console.log(typeof(isFollow));
+
+// let a;
+// console.log(typeof(a));
+
+// let b = null;
+// console.log(typeof(b));
+
+// object
+
+const Student = {
+    fullName : "saad",
+    age : 21,
+    cgpa : 2.8,
+};
+
+Student.age = 30;
+console.log(Student.age);
+
+console.log(Student["fullName"]);
+console.log(Student["cgpa"]);
---
+++ data type and variables/practice_questin_1/jawa_s.js
@@ -0,0 +1,17 @@
+const profile = {
+    userName : "shradhaKhapra",
+    isFollow : true,
+    ismessege : false,
+    posts : 195,
+    followers : 569,
+    following : 4,
+    email : "@shradhakaprah",
+};
+
+console.log(profile.userName);
+console.log(profile.isFollow);
+console.log(profile.ismessege);
+console.log(profile.posts);
+console.log(profile.followers);
+console.log(profile.following);
+console.log(profile.email);
