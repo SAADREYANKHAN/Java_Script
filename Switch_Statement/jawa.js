4fee5694efa9108ba4d25ffebe338f102fdd85b1
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-07 10:15:38 -0800

    Switch Statement Practice

---
+++ Switch_Statement/jawa.js
@@ -0,0 +1,13 @@
+let marks = 95;
+switch (true) {
+    case (marks >= 90 && marks <= 100):
+        console.log("Grade A");
+        break;
+
+    case (marks >=80 && marks < 90):
+        console.log("Grade B");
+        break;
+
+    default:
+        console.log("Fail");
+}
