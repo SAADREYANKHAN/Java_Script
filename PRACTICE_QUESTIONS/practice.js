0271efe05d1245e1fecb37c3a74deb52464ac7dc
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-08 00:04:48 -0800

    add students grade

--- PRACTICE_QUESTIONS/practice.js
+++ PRACTICE_QUESTIONS/practice.js
@@ -1,9 +1,25 @@
 // prompt is like a messege which display on website with some input information;
-console.log("Check if the number is multiple of 5 or not");
-let num = prompt("Enter any number");
-if(num % 5 ===0){
-    console.log(num,"is multiple of 5");
+// console.log("Check if the number is multiple of 5 or not");
+// let num = prompt("Enter any number");
+// if(num % 5 ===0){
+//     console.log(num,"is multiple of 5");
 
+// }else{
+//     console.log(num,"is NOT multiple of 5");
+// }
+
+
+// give grades to the students ;
+
+let marks = prompt("Enter student marks");
+
+if(marks >=80 && marks <=100){
+    console.log("GRADE A");
+}else if(marks >= 70 && marks <80){
+    console.log("GRADE B");
+}
+else if(marks >=50 && marks <70){
+    console.log("GRADE C");
 }else{
-    console.log(num,"is NOT multiple of 5");
+    console.log("Fail try agin in next exam");
 }
