fa690f3fa9e7501544987295a0542ec3082964e9
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-07 23:59:30 -0800

    check multple of 5 by if else

---
+++ PRACTICE_QUESTIONS/practice.js
@@ -0,0 +1,9 @@
+// prompt is like a messege which display on website with some input information;
+console.log("Check if the number is multiple of 5 or not");
+let num = prompt("Enter any number");
+if(num % 5 ===0){
+    console.log(num,"is multiple of 5");
+
+}else{
+    console.log(num,"is NOT multiple of 5");
+}
