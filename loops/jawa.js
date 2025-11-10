33a3955cdb0192870dbbfefb0579c4742c0411a3
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-10 06:46:38 -0800

    Add for of loop

--- loops/jawa.js
+++ loops/jawa.js
@@ -1,7 +1,19 @@
 // SUM OF N NUMBERS;
-let n = prompt("enter the value of n");
-let sum = 0;
-for(let i = 1; i<=n;i++){
-    sum = sum + i;
+// let n = prompt("enter the value of n");
+// let sum = 0;
+// for(let i = 1; i<=n;i++){
+//     sum = sum + i;
+// }
+// console.log(" sum = ",sum);
+
+// FOR OF LOOP;
+
+let str = prompt("Enter the string");
+let size = 0;
+
+for(let char of str){
+    console.log("char = ",char);
+    size++;
 }
-console.log(" sum = ",sum);
+
+console.log("size of the string is = ",size);
