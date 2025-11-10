41267ee815aa8b3fe90eb1d9ab26aa7d120c8fbc
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-10 08:09:59 -0800

    Guess game practice question

--- Loop Practice question/jawa.js
+++ Loop Practice question/jawa.js
@@ -1,7 +1,18 @@
 // print numbers from 0 to 100;
-let num;
-for(num = 0;num<=100;num++){
-    if(num % 2 == 0){
-        console.log("even number = ",num);
-    }
+// let num;
+// for(num = 0;num<=100;num++){
+//     if(num % 2 == 0){
+//         console.log("even number = ",num);
+//     }
+// }
+
+// Guess the number game practice;
+
+let gameNum = prompt("Enter the game number");
+let userNum = prompt("Guess the number");
+
+while(userNum != gameNum){
+    userNum = prompt("You entered the wrong number. Guess again");
 }
+
+console.log("Conratulation, you enter the right number");
