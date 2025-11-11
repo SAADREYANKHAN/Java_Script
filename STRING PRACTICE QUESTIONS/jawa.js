a348ed5203fbf287d459ff934d161911c1b0e0ae
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-11 14:45:52 -0800

    Reverse the String

--- STRING PRACTICE QUESTIONS/jawa.js
+++ STRING PRACTICE QUESTIONS/jawa.js
@@ -1,3 +1,7 @@
-let user = prompt("Enter user name without space");
-let res = "@".concat(user) + user.length;
-console.log(res.toUpperCase());
+// let user = prompt("Enter user name without space");
+// let res = "@".concat(user) + user.length;
+// console.log(res.toUpperCase());
+
+let str = "saad reyan";
+let reverse = str.split('').reverse().join('');
+console.log(reverse);
