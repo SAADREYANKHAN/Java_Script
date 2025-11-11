fd056db83ef5908ce78daddfca1d1e37f4afbe63
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-11 13:13:47 -0800

    Add Templete Literal String

--- STRINGS/jawa.js
+++ STRINGS/jawa.js
@@ -5,3 +5,13 @@ console.log(str.length);
 for(let i = 0;i<str.length;i++){
     console.log("index",i,"=",str[i]);
 }
+
+// Templete Literals;
+
+let string = {
+    obj : "pen",
+    price : 100,
+};
+
+let str1 = `the object is ${string.obj} and its price is ${string.price}`;
+console.log(str1);
