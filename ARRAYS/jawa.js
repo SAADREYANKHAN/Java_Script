d06389cf45d96ce5749a43ccc92d966a30ae9c97
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-14 08:31:46 -0800

    Basic concept of array

---
+++ ARRAYS/jawa.js
@@ -0,0 +1,11 @@
+let marks = [1,2,3,4,5,6,7,8,9,10];
+marks[5] = "saad";
+console.log(marks);
+
+// use loop on the array;
+// here uing for _of loop is best option;
+
+let cities = ["pesh","charsada","islam"];
+for(let city of cities){
+    console.log(city.toUpperCase());
+}
