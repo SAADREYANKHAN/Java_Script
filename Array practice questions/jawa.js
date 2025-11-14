ad1b1146ee7867e65e7998ab1f733c57c43c1791
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-14 09:30:18 -0800

    Find offer on the original price

--- Array practice questions/jawa.js
+++ Array practice questions/jawa.js
@@ -1,15 +1,26 @@
 // find the average marks of the entire class;
 
-let sum = 0,average = 0;
-let marks = [85,97,44,37,76,60]; 
+// let sum = 0,average = 0;
+// let marks = [85,97,44,37,76,60]; 
 
-// Find sum of the marks
-for(let i of marks){
-    sum += i;
+// // Find sum of the marks
+// for(let i of marks){
+//     sum += i;
+// }
+
+// console.log("sum of the marks = ",sum);
+// // now find the average
+
+// average = sum/marks.length;
+// console.log(`Average marks of the class is = ${average}`);
+
+// Find the offer price after 10% off on original price of items;
+
+let items = [250,645,300,900,50];
+
+for(let i = 0; i< items.length;i++){
+    console.log(`Original price of the item is = ${items[i]}`);
+    let offer = items[i]/10;
+    items[i] = items[i] - offer;
+    console.log(`Ater offer the price of item is = ${items[i]}`);
 }
-
-console.log("sum of the marks = ",sum);
-// now find the average
-
-average = sum/marks.length;
-console.log(`Average marks of the class is = ${average}`);
