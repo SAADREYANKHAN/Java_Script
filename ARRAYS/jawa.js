39a7c28bcebe61685e81acdc28352744d90674b6
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-19 08:24:22 -0800

    Add push(),pop(),toString() Function

--- ARRAYS/jawa.js
+++ ARRAYS/jawa.js
@@ -1,11 +1,43 @@
-let marks = [1,2,3,4,5,6,7,8,9,10];
-marks[5] = "saad";
-console.log(marks);
+// let marks = [1,2,3,4,5,6,7,8,9,10];
+// // marks[5] = "saad";
+// // console.log(marks);
+// for(let i = 0;i<marks.length;i++){
+//     console.log(marks[i]);
+// }
 
-// use loop on the array;
-// here uing for _of loop is best option;
+// // use loop on the array;
+// // here uing for _of loop is best option;
 
-let cities = ["pesh","charsada","islam"];
-for(let city of cities){
-    console.log(city.toUpperCase());
-}
+// let cities = ["pesh","charsada","islam"];
+// for(let city of cities){
+//     console.log(city.toUpperCase());
+// }
+
+// METHODS IN ARRAY LIKE {(push), (pop),(tostring)};
+
+// Push()_Function;
+
+let Items = ["pen","tomato","mango"];
+console.log(Items);
+console.log("After the push() function the list become");
+
+Items.push("stobery","banana","apple");
+console.log(Items);
+
+// Add Pop() Function;
+console.log("After pop");
+let deleteItem = Items.pop();
+console.log(Items);
+console.log(deleteItem);
+
+console.log("After pop");
+deleteItem = Items.pop();
+console.log(Items);
+console.log(deleteItem);
+
+// Add toString() function
+console.log("Convert to string");
+console.log(Items.toString());
+
+// Concate method;
+
