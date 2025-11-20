810e9dd02c09625ae0d51a37629440041e44b59a
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-11-20 06:27:41 -0800

    Concept of functions in JS

---
+++ FUNCTIONS/jawa.js
@@ -0,0 +1,27 @@
+// Functions and Methods;
+// function car(){
+//     string = prompt("Enter car color");
+
+//     if(string === "black"){
+//         console.log("it is sevic new version");
+//     }else{
+//         console.log("it is the old version");
+//     }
+// }
+
+// car();
+
+
+// parameterized function;
+
+function fun(fun){
+    console.log(fun);
+}
+fun("hi am practicing jawa script");
+
+function sum(a,b){
+    let sum = a + b;
+   // console.log("The sum is = ",sum);
+   return sum;
+}
+console.log(sum(10,20));
